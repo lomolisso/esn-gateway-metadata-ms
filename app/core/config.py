7967bb5485f898306214d48e36aa12c40bba8394
Raul de la Fuente <raul.delafuente@ing.uchile.cl>
2024-08-27 15:54:13 -0400

    updated for latency experiment

--- app/core/config.py
+++ app/core/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 # Retrieve enviroment variables from .env file
 load_dotenv()
 
-SECRET_KEY: str = os.environ.get("SECRET_KEY")
-METADATA_MICROSERVICE_HOST: str = os.environ.get("METADATA_MICROSERVICE_HOST")
-METADATA_MICROSERVICE_PORT: int = int(os.environ.get("METADATA_MICROSERVICE_PORT"))
+SECRET_KEY: str = os.environ.get("SECRET_KEY", "secret_key")
+METADATA_MICROSERVICE_HOST: str = os.environ.get("METADATA_MICROSERVICE_HOST", "127.0.0.1")
+METADATA_MICROSERVICE_PORT: int = int(os.environ.get("METADATA_MICROSERVICE_PORT", 8007))
 
 TIMEZONE: str = os.environ.get("TIMEZONE", "Chile/Continental")
 
